868e389801efcc1f445973fa0dc4dc674cd485dc
Dan Kubb <github@dan.kubb.ca>
2020-07-28 09:08:18 -0700

    Add BankAccount.split static method

--- spec/bank_account_spec.js
+++ spec/bank_account_spec.js
@@ -3,8 +3,44 @@
 const BankAccount = require('../src/bank_account')
 
 describe('BankAccount', () => {
+  const zero = [
+    ' _ ',
+    '| |',
+    '|_|'
+  ]
+
+  const one = [
+    '   ',
+    '  |',
+    '  |'
+  ]
+
+  const zero_one = zero.map((chunk, index) => chunk + one[index])
+
   it('exists', () => {
     expect(BankAccount).toBeDefined()
+  })
+
+  describe('.split', () => {
+    describe('when the text is 0', () => {
+      it('returns zero', () => {
+        expect(BankAccount.split(zero.join('\n'))).toEqual([zero.join('')])
+      })
+    })
+
+    describe('when the text is 1', () => {
+     it('returns one', () => {
+        expect(BankAccount.split(one.join('\n'))).toEqual([one.join('')])
+      })
+    })
+
+    describe('when the text is 01', () => {
+      it('returns zero one', () => {
+        const digits = [zero.join(''), one.join('')]
+
+        expect(BankAccount.split(zero_one.join('\n'))).toEqual(digits)
+      })
+    })
   })
 
   describe('constructor', () => {
--- src/bank_account.js
+++ src/bank_account.js
@@ -3,6 +3,26 @@
 /** A class representing a Bank Account. */
 class BankAccount {
 
+  /* Split the text into an Array of digits
+   *
+   * @param {string} text
+   *
+   * @returns {Array<string>}
+   */
+  static split(text) {
+    // Split the text into each line
+    return text.split('\n').reduce((digits, line) => {
+
+      // Match each line in group of 3 characters
+      line.match(/.{3}/g).forEach((chars, lineNo) => {
+        // Append characters to the digit
+        digits[lineNo] ? digits[lineNo] += chars : digits[lineNo] = chars
+      })
+
+      return digits
+    }, [])
+  }
+
   /* Initialize the Bank Account
    *
    * @param {(string|string[])} number
