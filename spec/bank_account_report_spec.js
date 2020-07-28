4393138639221eca32164cede8ed9c72cd4624d3
Dan Kubb <github@dan.kubb.ca>
2020-07-28 10:36:29 -0700

    Add BankAccountReport class

---
+++ spec/bank_account_report_spec.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const BankAccountReport = require('../src/bank_account_report')
+
+describe('BankAccountReport', () => {
+  it('exists', () => {
+    expect(BankAccountReport).toBeDefined()
+  })
+})
---
+++ src/bank_account_report.js
@@ -0,0 +1,7 @@
+'use strict'
+
+/** A class representing a Bank Account Report */
+class BankAccountReport {
+}
+
+module.exports = BankAccountReport
