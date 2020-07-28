33b97fde963cc1e767188c5d777ba7156d655f3c
Dan Kubb <github@dan.kubb.ca>
2020-07-28 10:38:55 -0700

    Add BankAccountReport constructor

--- spec/bank_account_report_spec.js
+++ spec/bank_account_report_spec.js
@@ -3,7 +3,21 @@
 const BankAccountReport = require('../src/bank_account_report')
 
 describe('BankAccountReport', () => {
+  const contents = ''
+
+  const report = new BankAccountReport(contents)
+
   it('exists', () => {
     expect(BankAccountReport).toBeDefined()
+  })
+
+  describe('constructor', () => {
+    it('returns a BankAccountReport instance', () => {
+      expect(report).toBeInstanceOf(BankAccountReport)
+    })
+
+    it('sets the contents', () => {
+      expect(report.contents).toBe(contents)
+    })
   })
 })
--- src/bank_account_report.js
+++ src/bank_account_report.js
@@ -2,6 +2,14 @@
 
 /** A class representing a Bank Account Report */
 class BankAccountReport {
+
+  /* Initialize a Bank Account Report
+   *
+   * @param {string} contents the file contents
+   */
+  constructor(contents) {
+    this.contents = contents
+  }
 }
 
 module.exports = BankAccountReport
